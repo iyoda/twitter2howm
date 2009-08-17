53d0af5a18d216fde6fa97355d5fe0a51d718d00
IYODA Atsushi <iyoda-github@atikoro.net>
2009-08-18 02:16:44 +0900

    get pages

--- twitter2howm.py
+++ twitter2howm.py
@@ -2,63 +2,74 @@
 import os, time
 import twitter
 
-def run(userid, dir, last_created_utc, count = None):
+def run(userid, dir, since_id, count = None):
     api = twitter.Api()
-    since = time.strftime('%a %b %d %H:%M:%S %Y',
-                          time.localtime(last_created_utc))
-    statuses = api.GetUserTimeline(userid, count, since)
-    statuses.reverse()
-    for s in statuses:
-        # ファイル名
-        filename = 'tw-%d.howm' % s.id
-        # howm日時
-        created_utc = time.mktime(time.strptime(s.created_at,
-                                                '%a %b %d %H:%M:%S +0000 %Y'))
-        if last_created_utc < created_utc:
-            # 最終取得日時
-            last_created_utc = created_utc
-        created = time.localtime(created_utc - time.timezone)
-        howm_time = time.strftime('[%Y-%m-%d %H:%M]', created)
+    last_status_id = None
+    for page in range(0,16):
+        statuses = api.GetUserTimeline(userid, count = count,
+                                       since_id = since_id, page = page)
+        if len(statuses) == 0:
+            return last_status_id
+        print 'page=%d' % page
+        for s in statuses:
+            write_howm(dir, s)
+            if s.id > last_status_id:
+                last_status_id = s.id
 
-        year = time.strftime('%Y', created)
-        dirname = dir + '/' + year
+    return last_status_id
 
-        # ファイルに出力する
-        try:
-            os.makedirs(dirname)
-        except OSError:
-            None
-        f = open(dirname + '/' + filename, 'w')
-        # euc-jp で出力する
-        print >>f, '= tw:' + howm_time + '. ' + s.text.encode('utf-8')
-        f.close()
+# .howmを書き込む
+def write_howm(dir, s):
+    #print s.id
+    #print s.text.encode('utf-8')
+    # ファイル名
+    filename = 'tw-%d.howm' % s.id
+    # howm日時
+    created_utc = time.mktime(time.strptime(s.created_at,
+                                            '%a %b %d %H:%M:%S +0000 %Y'))
+    created = time.localtime(created_utc - time.timezone)
+    howm_time = time.strftime('[%Y-%m-%d %H:%M]', created)
 
-    return last_created_utc
+    year = time.strftime('%Y', created)
+    dirname = dir + '/' + year
 
-# キャッシュファイルから最終ステータス時刻を取得
+    # ファイルに出力する
+    try:
+        os.makedirs(dirname)
+    except OSError:
+        None
+    f = open(dirname + '/' + filename, 'w')
+    # euc-jp で出力する
+    print >>f, '= tw:' + howm_time + '. ' + s.text.encode('utf-8')
+    f.close()
+
+    return s.id
+
+# キャッシュファイルから最終ステータスIDを取得
 def read_cache(filename):
     try:
         f = open(filename, 'r')
         line = f.readline()
-        last_created_utc = eval(line)
+        last_status_id = eval(line)
         f.close()
     except IOError:
-        last_created_utc = 0
+        last_status_id = 1
 
-    return last_created_utc
+    return last_status_id
 
-# キャッシュファイルに最終ステータス時刻を保存
-def save_cache(filename, last_created_utc):
-    f = open(filename, 'w')
-    print >>f, repr(last_created_utc + 1)
-    f.close()
+# キャッシュファイルに最終ステータスIDを保存
+def save_cache(filename, last_status_id):
+    if last_status_id != None:
+        f = open(filename, 'w')
+        print >>f, repr(last_status_id)
+        f.close()
 
 if __name__=='__main__':
     userid = 'iyoda'
-    count = 2000
+    count = 200
     dir = '/Users/iyoda/Documents/howm'
     cache_filename = '.twitter2howm.cache'
 
-    last_created_utc = read_cache(dir + '/' + cache_filename)
-    last_created_utc = run(userid, dir, last_created_utc, count)
-    save_cache(dir + '/' + cache_filename, last_created_utc)
+    last_status_id = read_cache(dir + '/' + cache_filename)
+    status_id = run(userid, dir, last_status_id, count)
+    save_cache(dir + '/' + cache_filename, status_id)
